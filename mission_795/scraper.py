57d69eb5a17c6d4ffaa46452c6d02f3e3ea65916
Anthony Santos <anthony.a.santos@gmail.com>
2014-12-20 03:48:40 -0800

    Parsing the printer-friendly version of the page, since it's much simpler

--- mission_795/scraper.py
+++ mission_795/scraper.py
@@ -6,18 +6,20 @@ import json
 import requests
 import turbotlib
 
-source_url = "http://nbt.tj/en/banking_system/credit_org.php"
+# The printer-friendly version of the page is much easier to parse
+source_url = "http://nbt.tj/en/banking_system/credit_org.php?print=Y"
 sample_date = str(date.today())
 turbotlib.log("Starting scrape...") # Optional debug logging
 response = requests.get(source_url)
 html = response.content
-doc = BeautifulSoup(html)
-table = # TODO, gross
+doc = BeautifulSoup(html, "lxml")
+table = doc.table.table.table
+print(table)
 for n in range(0,20):
     data = {"number": n,
             "company": "Company %s Ltd" % n,
             "message": "Hello %s" % n,
-            "sample_date": datetime.datetime.now().isoformat(),
+            "sample_date": sample_date,
             "source_url": "http://somewhere.com/%s" % n}
     # The Turbot specification simply requires us to output lines of JSON
     print json.dumps(data)
